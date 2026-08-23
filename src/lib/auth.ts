6cdaeea95d2319fa6de7279b15a62565e841bf39
saksham762007 <samrathore07062007@gmail.com>
2026-08-23 19:43:51 +0530

    feat: implement authentication logic and sign-in/sign-up pages using Better Auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,11 +20,16 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
   },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
 });
