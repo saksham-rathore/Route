4e6cd8dd2615644eeed85cbe0c030a9a7b0a7655
sammy <samrathore07062007@gmail.com>
2026-09-09 12:14:28 +0530

    feat: initialize singleton Prisma client and configure Better Auth with database adapter and social providers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,18 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma/client";
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
--- src/lib/validator/project.ts
+++ src/lib/validator/project.ts
@@ -1,6 +1,7 @@
-import * as z from "zod"; 
+import * as z from "zod";
 
-const Projects = z.object({
-    Project_name: z.string(),
-    
-})
+const ProjectSchema = z.object({
+  projectName: z.string().min(1),
+  projectDomain: z.string().min(1),
+  projectId: z.string(),
+});
